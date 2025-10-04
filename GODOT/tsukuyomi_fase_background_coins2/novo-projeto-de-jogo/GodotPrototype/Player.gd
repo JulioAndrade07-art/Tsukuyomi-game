44f2e78bce01d5c00deb5eb826786801e4fd47cc
Nathangch <nathan.gabriel.180903@gmail.com>
2025-10-04 16:18:31 -0300

    O QUE MUDOU

---
+++ GODOT/tsukuyomi_fase_background_coins2/novo-projeto-de-jogo/GodotPrototype/Player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+@export var speed: float = 200.0
+@export var jump_force: float = -400.0
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+    if not is_on_floor():
+        velocity.y += gravity * delta
+
+    var input_dir = Input.get_axis("ui_left", "ui_right")
+    velocity.x = input_dir * speed
+
+    if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+        velocity.y = jump_force
+
+    move_and_slide()
---
+++ GODOT/tsukuyomi_fase_background_coins2/novo-projeto-de-jogo/main.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+var score: int = 0
+
+func _ready():
+	var coin = $Coin
+	coin.connect("collected", Callable(self, "_on_coin_collected"))
+
+func _on_coin_collected():
+	score += 1
+	print("Moeda coletada! Score: ", score)
