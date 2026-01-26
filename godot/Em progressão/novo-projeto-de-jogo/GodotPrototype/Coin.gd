d6e2fda49487a0e81157f9c778a87aca51c22eac
Nathangch <nathan.gabriel.180903@gmail.comgit config --global user.email nathan.gabriel.180903@gmail.com>
2026-01-26 18:56:24 -0300

    novo gdd

--- godot/Em progressão/novo-projeto-de-jogo/GodotPrototype/Coin.gd
+++ godot/Em progressão/novo-projeto-de-jogo/GodotPrototype/Coin.gd
@@ -3,9 +3,9 @@ extends Area2D
 signal collected
 
 func _ready():
-    connect("body_entered", Callable(self, "_on_body_entered"))
+	connect("body_entered", Callable(self, "_on_body_entered"))
 
 func _on_body_entered(body):
-    if body.is_in_group("player"):
-        emit_signal("collected")
-        queue_free()
+	if body.is_in_group("player"):
+		emit_signal("collected")
+		queue_free()
